17f576d800c3598ff8e201f38a78a8adbf53e0c7
root <root@ampere01.pouliot.net>
2019-10-30 18:37:48 -0400

    script fixes

--- bin/packer_variables.sh
+++ bin/packer_variables.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
-
+source /etc/kolla/admin-openrc.sh
 OS_SOURCE_IMAGE=`openstack image list | grep 'ubuntu-18.04'| awk '{print $2}'`
 OS_NETWORKS_ID=`openstack network list | grep 'demo-net'| awk '{print $2}'`
 OS_FLOATING_IP_POOL='public1'
 
 echo $OS_SOURCE_IMAGE
-echo $OS_NETWORK_ID
-echo $OS_FLOATING_IP
+echo $OS_NETWORKS_ID
+echo $OS_FLOATING_IP_POOL
 
 
