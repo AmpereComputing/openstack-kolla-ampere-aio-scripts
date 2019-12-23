3388c2c0c71be4944d73b0381880ca395a9c2766
Peter Pouliot <peter@pouliot.net>
2019-12-23 12:14:16 -0500

    Changing Packer version and cleanup

--- 008_packer.sh
+++ 008_packer.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 
+PACKER_VERSION='1.5.1'
+
 source /etc/kolla/admin-openrc.sh
 apt-get install unzip -y
 cd /usr/local/bin
-wget -cv https://releases.hashicorp.com/packer/1.4.4/packer_1.4.4_linux_arm64.zip
-unzip -u packer_1.4.4_linux_arm64.zip
-rm -rf packer_1.4.4_linux_arm64.zip
+wget -cv https://releases.hashicorp.com/packer/$PACKER_VERSION/packer_$PACKER_VERSION_linux_arm64.zip
+unzip -u packer_$PACKER_VERSION_linux_arm64.zip
+rm -rf packer_$PACKER_VERSION_linux_arm64.zip
 chmod +x /usr/local/bin/packer
 /usr/local/bin/packer --version
 exit
