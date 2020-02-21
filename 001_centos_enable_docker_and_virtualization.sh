d53ba3be32f9111030d938623bd799fb0e32ebc5
Peter Pouliot <peter@pouliot.net>
2020-02-20 22:37:02 -0500

    Create 001_centos_enable_docker_and_virtualization.sh

---
+++ 001_centos_enable_docker_and_virtualization.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+PROJECT_DIR=`pwd`
+LOGFILE=$PROJECT_DIR/centos_kolla.log
+exec >> $LOGFILE 2>&1
+
+TOOLS="screen rsync git curl"
+DOCKER="docker-ce docker-ce-cli containerd.io"
+PYTHON3="python-devel libselinux-python platform-python-pip"
+DEVEL="libffi-deviel gcc openssl-devel"
+VIRT="qemu-kvm qemu-img libvirt virt-install libvirt-client iscsi-initiator-utils"
+
+# Add Docker package repositories
+dnf config-manager --add-repo=https://download.docker.com/linux/centos/docker-ce.repo
+
+# Update package cache and upgrade packages
+dnf check-update -y && dnf update -y
+
+echo "Installing tooling"
+dnf install -y $TOOLS
+
+echo "Installing Pythong Development"
+dnf install -y $PYTHON3 $DEVEL
+
+echo "Installing and Testing Docker"
+dnf install -y $DOCKER
+docker info
+
+echo "Installing Virtualization"
+dnf install -y $VIRT
+
+echo "Comment out /etc/hosts line that begins with 127.0.1.1"
+sed -i 's/^127.0.1.1/#127.0.1.1/' /etc/hosts
