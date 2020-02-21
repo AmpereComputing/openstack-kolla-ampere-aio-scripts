f22a85209dc45b7b045a0bbb666123ea20961baa
Peter Pouliot <peter@pouliot.net>
2020-02-20 23:42:21 -0500

    Update 001_centos_enable_docker_and_virtualization.sh

--- 001_centos_enable_docker_and_virtualization.sh
+++ 001_centos_enable_docker_and_virtualization.sh
@@ -6,12 +6,13 @@ exec >> $LOGFILE 2>&1
 
 TOOLS="screen rsync git curl"
 DOCKER="docker-ce docker-ce-cli containerd.io"
-PYTHON3="python-devel libselinux-python platform-python-pip"
+PYTHON3="platform-python-devel python3-libselinux python3 python3-pip python3-setuptools python3-virtualenv"
 DEVEL="libffi-devel gcc openssl-devel"
-VIRT="qemu-kvm qemu-img libvirt virt-install libvirt-client iscsi-initiator-utils"
+VIRT="qemu-kvm qemu-img libvirt virt-install libvirt-client iscsi-initiator-utils libvirt-daemon-kvm"
 
-# Add Docker package repositories
+# Add Docker and EPEL-Release package repositories
 dnf config-manager --add-repo=https://download.docker.com/linux/centos/docker-ce.repo
+dnf install -y epel-release
 
 # Update package cache and upgrade packages
 dnf check-update -y && dnf update -y
@@ -19,11 +20,15 @@ dnf check-update -y && dnf update -y
 echo "Installing tooling"
 dnf install -y $TOOLS
 
-echo "Installing Pythong Development"
+echo "Installing Python Development"
 dnf install -y $PYTHON3 $DEVEL
+alternatives --set python /usr/bin/python3
+alternatives --set pip /usr/bin/pip3
 
-echo "Installing and Testing Docker"
+echo "Installing Enabling and Testing Docker"
 dnf install -y --nobest $DOCKER
+systemctl enable docker
+systemctl start docker
 docker info
 
 echo "Installing Virtualization"
