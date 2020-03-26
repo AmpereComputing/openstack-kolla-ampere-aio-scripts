ee623c50148fd2f90f01f8775779c472cca86217
Peter J. Pouliot <peter@pouliot.net>
2020-03-26 09:15:34 -0400

    reverting to config for my local setup

--- 009_k3OS_packer_image.sh
+++ 009_k3OS_packer_image.sh
@@ -1,21 +1,20 @@
 #!/usr/bin/env
 
 PROJECT_DIR=`pwd`
-LOGFILE=$PROJECT_DIR/009_k3os_packer_image.log
+LOGFILE=${PROJECT_DIR}/009_k3os_packer_image.log
 exec >> $LOGFILE 2>&1
 
 source /etc/kolla/admin-openrc.sh
-
-OS_SOURCE_IMAGE=`openstack image list | grep 'ubuntu-18.04'| awk '{print $2}'`
-OS_NETWORKS_ID=`openstack network list | grep 'demo-net'| awk '{print $2}'`
-OS_FLOATING_IP_POOL='public1'
+export OS_SOURCE_IMAGE=`openstack image list | grep 'ubuntu-18.04'| awk '{print $2}'`
+export OS_NETWORKS_ID=`openstack network list | grep 'demo-net'| awk '{print $2}'`
+export OS_FLOATING_IP_POOL='public1'
 
 echo $OS_SOURCE_IMAGE
 echo $OS_NETWORK_ID
 echo $OS_FLOATING_IP
 
 cd /opt
-git clone https://github.com/ppouliot/k3os
+git clone https://github.com/rancher/k3os
 cd /opt/k3os/package/packer/openstack
 packer validate template-arm64.json
 packer build template-arm64.json
