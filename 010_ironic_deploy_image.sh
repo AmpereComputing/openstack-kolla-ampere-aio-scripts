3b5106ba9585b4712859eaafdce9207688bb5747
Peter Pouliot <ppouliot@amperecomputing.com>
2020-04-24 14:39:00 -0700

    010_ironic_deploy_image.sh

---
+++ 010_ironic_deploy_image.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+PROJECT_DIR=`pwd`
+LOGFILE=$PROJECT_DIR/010_ironic_deploy_image.log
+exec >> $LOGFILE 2>&1
+
+# ensure tooling is present
+apt-get update -y && apt-get install qemu-utils kpartx -y 
+
+# Change to /opt and download ironic tooling form src
+cd /opt
+
+git clone https://opendev.org/openstack/ironic-python-agent
+git clone https://opendev.org/openstack/diskimage-builder
+git clone https://opendev.org/openstack/ironic-python-agent-builder
+
+# Install 
+pip install -U ./diskimage-builder
+pip install -U ./ironic-python-agent-builder
+
+# Create the ironic config
+mkdir -p /etc/kolla/config/ironic
+
+wget -c https://alpha.release.flatcar-linux.net/arm64-usr/current/flatcar_production_pxe.vmlinuz -O /etc/kolla/config/ironic/flatcar_production_pxe.vmlinuz
+wget -c https://alpha.release.flatcar-linux.net/arm64-usr/current/flatcar_production_pxe_image.cpio.gz -O /etc/kolla/config/ironic/flatcar_production_pxe_image.cpio.gz
